8d16aa2ff6566de0da73874118963e6ddc7902a8
mhhasanmehedi <mmmehedi53@gmail.com>
2022-11-08 00:43:30 +0600

    Building the header items

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,25 @@
 import Image from "next/image";
+import HeaderItem from "./HeaderItem";
+import {
+  CheckBadgeIcon,
+  CircleStackIcon,
+  HomeIcon,
+  LightBulbIcon,
+  MagnifyingGlassIcon,
+  UserIcon,
+} from "@heroicons/react/24/outline";
 
 function Header() {
   return (
-    <header>
-      <h1>This is the header</h1>
+    <header className="flex flex-col items-center sm:flex-row m-5 justify-between h-auto">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
+        <HeaderItem title="HOME" Icon={HomeIcon} />
+        <HeaderItem title="TRENDING" Icon={LightBulbIcon} />
+        <HeaderItem title="VERIFIED" Icon={CheckBadgeIcon} />
+        <HeaderItem title="COLLECTIONS" Icon={CircleStackIcon} />
+        <HeaderItem title="SEARCH" Icon={MagnifyingGlassIcon} />
+        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
+      </div>
       <Image
         className="object-contain"
         src="https://links.papareact.com/ua6"
