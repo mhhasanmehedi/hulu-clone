3b04600019c24761eb1a527514ae828f1186751b
mhhasanmehedi <mmmehedi53@gmail.com>
2022-11-08 16:08:19 +0600

    Building the results components

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -1,11 +1,15 @@
 import { HandThumbUpIcon } from "@heroicons/react/24/outline";
 import Image from "next/image";
+import { forwardRef } from "react";
 
-const Thumbnail = ({ result }) => {
-  console.log(result);
+// eslint-disable-next-line react/display-name
+const Thumbnail = forwardRef(({ result }, ref) => {
   const BASE_URL = "https://image.tmdb.org/t/p/original/";
   return (
-    <div className="group cursor-pointer p-2 transition duration-200 ease-in transform  sm:hover:scale-105 hover:z-50">
+    <div
+      ref={ref}
+      className="group cursor-pointer p-2 transition duration-200 ease-in transform  sm:hover:scale-105 hover:z-50"
+    >
       <Image
         layout="responsive"
         src={
@@ -30,6 +34,6 @@ const Thumbnail = ({ result }) => {
       </div>
     </div>
   );
-};
+});
 
 export default Thumbnail;
